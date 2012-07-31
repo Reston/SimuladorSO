9cf7111c1953104b569547241ce4cc910160fd02
José Ordoñez <restonjd@gmail.com>
2012-07-31 18:52:42 -0430

    ya muestra los resultados

--- sisope.py
+++ sisope.py
@@ -97,11 +97,43 @@ class SisOpe(wx.Frame):
 		#print str(len(self.listaAlta))+" + "+str(len(self.listaMedia))+" + "+str(len(self.listaBaja))+" = "+str(len(self.listaAlta)+len(self.listaMedia)+len(self.listaBaja))
 		
 		
-		detalles = "ID\tMemoria\tTiempo Inicial\tTiempo Final\tDuracion\tTiempo llegada\tPrioridad\n"
+		detalles = "ID\tEstado\t\tMemoria\tTiempo Inicial\tTiempo Final\tDuracion\tTiempo llegada\tPrioridad\n"
 		for self.tar in self.listaAlta:
-			detalles+= str(self.tar.idtar)+"\t"+str(self.tar.memoria)+"\t"+str(self.tar.getTiempoInicial())+"\t\t"+str(self.tar.getTiempoFinal())+"\t\t"+str(self.tar.getTiempoDuracion())+"\t"+str(self.tar.getTiempoLlegada())+"\tAlta\n"
+			if (self.tar.getTiempoFinal() >= int(self.tiempo.GetValue())):
+				self.tar.setEstado("En ejecucion")
+			else:
+				if self.tar.getTiempoDuracion()==0:
+					self.tar.setEstado("Suspendidas")
+				elif self.tar.getTiempoDuracion()!=0:
+					self.tar.setEstado("Terminado")
+			
+			detalles+= str(self.tar.getID())+"\t"+self.tar.getEstado()+"\t"+str(self.tar.getMemoria())+"\t"+str(self.tar.getTiempoInicial())+"\t\t"+str(self.tar.getTiempoFinal())+"\t\t"+str(self.tar.getTiempoDuracion())+"\t"+str(self.tar.getTiempoLlegada())+"\t\tAlta\n"
+		
+		detalles+= "\n"
+		for self.tar in self.listaMedia:
+			if (self.tar.getTiempoFinal() >= int(self.tiempo.GetValue())):
+				self.tar.setEstado("En ejecucion")
+			else:
+				if self.tar.getTiempoDuracion()==0:
+					self.tar.setEstado("Suspendidas")
+				elif self.tar.getTiempoDuracion()!=0:
+					self.tar.setEstado("Terminado")
+
+			detalles+= str(self.tar.getID())+"\t"+self.tar.getEstado()+"\t"+str(self.tar.getMemoria())+"\t"+str(self.tar.getTiempoInicial())+"\t\t"+str(self.tar.getTiempoFinal())+"\t\t"+str(self.tar.getTiempoDuracion())+"\t"+str(self.tar.getTiempoLlegada())+"\t\tMedia\n"
+		detalles+= "\n"
+
+		for self.tar in self.listaBaja:
+			if (self.tar.getTiempoFinal() >= int(self.tiempo.GetValue())):
+				self.tar.setEstado("En ejecucion")
+			else:
+				if self.tar.getTiempoDuracion()==0:
+					self.tar.setEstado("Suspendidas")
+				elif self.tar.getTiempoDuracion()!=0:
+					self.tar.setEstado("Terminado")
+
+			detalles+= str(self.tar.getID())+"\t"+self.tar.getEstado()+"\t"+str(self.tar.getMemoria())+"\t"+str(self.tar.getTiempoInicial())+"\t\t"+str(self.tar.getTiempoFinal())+"\t\t"+str(self.tar.getTiempoDuracion())+"\t"+str(self.tar.getTiempoLlegada())+"\t\tBaja\n"
 		 
-		resultado = Resultado(self,detalles)
+		resultado = Resultado(self,detalles, self.tiempo.GetValue(), self.ntarea.GetValue())
 		resultado.Show(True)
 		resultado.MakeModal(True)
 	
@@ -139,7 +171,28 @@ class SisOpe(wx.Frame):
 					else:
 						self.listaAlta.append(self.tar)
 						self.listaAlta.pop(self.listaAlta.index(self.tar))
+				if self.tar.tiempoFinal==x:
+					memoriaUsada -= self.tar.memoria
 
+			for self.tar in self.listaMedia:
+				if self.tar.tiempoLlegada==x:
+					if(self.revisar_memoria(memoriaTotal, memoriaUsada, self.tar.memoria)):
+						memoriaUsada += self.tar.memoria
+						self.set_timers(x, self.tar.tiempoDuracion)
+					else:
+						self.listaMedia.append(self.tar)
+						self.listaMedia.pop(self.listaMedia.index(self.tar))
+				if self.tar.tiempoFinal==x:
+					memoriaUsada -= self.tar.memoria
+
+			for self.tar in self.listaBaja:
+				if self.tar.tiempoLlegada==x:
+					if(self.revisar_memoria(memoriaTotal, memoriaUsada, self.tar.memoria)):
+						memoriaUsada += self.tar.memoria
+						self.set_timers(x, self.tar.tiempoDuracion)
+					else:
+						self.listaBaja.append(self.tar)
+						self.listaBaja.pop(self.listaBaja.index(self.tar))
 				if self.tar.tiempoFinal==x:
 					memoriaUsada -= self.tar.memoria
 		return None
@@ -177,25 +230,25 @@ class SisOpe(wx.Frame):
 
 class Resultado(wx.Frame):
 
-	def __init__(self, parent, detalles):
+	def __init__(self, parent, detalles, tiempo, numtareas):
 		wx.Frame.__init__(self, parent, wx.NewId(), "Resultados - SimuladorSO",
-							pos=(10,140), size=(600,400))
+							pos=(10,140), size=(650,400))
 		self.Bind(wx.EVT_CLOSE, self.al_cerrar)
 
 		#panel
 		self.panel = wx.Panel(self)
 		
 		#textos estaticos
-		self.cant = wx.StaticText(self.panel, -1, "Cantidad de tareas ejecutadas:", pos=(10,10))
-		self.time = wx.StaticText(self.panel, -1, "Tiempo total de ejecución:".decode('utf-8'),
-									pos=(10,25))
+		self.cant = wx.StaticText(self.panel, -1, "Cantidad de tareas ejecutadas:"+tiempo,
+								  pos=(10,10))
+		self.time = wx.StaticText(self.panel, -1, "Tiempo total de ejecucion:"+numtareas, pos=(10,25))
 		self.detalles = wx.StaticText(self.panel, -1, "Detalles de las tareas:",pos=(10,60))
 
 		#linea estatica
 		self.line = wx.StaticLine(self.panel, pos=(10, 50), size=(500,1))
 				
 		#caja de texto de los detalles
-		self.cajatext = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE, pos=(10,80), size=(550,210))
+		self.cajatext = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE, pos=(10,80), size=(600,210))
 		self.cajatext.SetValue(detalles)
 		#botones
 		bVolver = wx.Button(self.panel,label="Volver",pos=(430,300),size=(60,30))
